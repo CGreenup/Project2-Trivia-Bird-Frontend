5504aa703654b270221ea495d6a40bca8433b237
June Greenup <cgreenup2000@gmail.com>
2021-08-12 00:41:05 -0500

    added question retrieval API

---
+++ src/remote/questionApi.ts
@@ -0,0 +1,16 @@
+import { question } from "../models/question";
+import questionClient from "./questionClient";
+
+export const getQuestion = async (difficulty:string) => {
+    const response = await questionClient.get<question>(difficulty + "&type=multiple")
+    if(response.status == 200){
+        let data = response.data;
+        //debug
+        console.log("This is the data returned");
+        console.log(data)
+        return data;
+    }
+    else{
+        console.log("ERROR CONNECTING TO API")
+    }
+}
--- src/remote/questionClient.ts
+++ src/remote/questionClient.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const questionClient = axios.create({
-    baseURL:'https://opentdb.com/api.php',
+    baseURL:'https://opentdb.com/api.php?amount=1&difficulty=',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
+export default questionClient
