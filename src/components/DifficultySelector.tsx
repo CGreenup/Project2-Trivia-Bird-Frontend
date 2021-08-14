3365b0928350150725613bf4d34b3ba1aae4b280
June Greenup <cgreenup2000@gmail.com>
2021-08-13 23:49:41 -0500

    Clicking a difficulty and hitting start now redirects to game/difficulty

--- src/components/DifficultySelector.tsx
+++ src/components/DifficultySelector.tsx
@@ -1,44 +1,55 @@
 import React, { useState } from "react";
-import { getQuestion } from "../remote/questionApi";
+import { Redirect } from "react-router-dom";
 import DifficultyButton from "./DifficultyButton";
 
 const DifficultySelector:React.FC<unknown> = () => {
     const [isDisabled, setDisabled] = useState(true)
-    const [propValue, setValue] = useState("");
+    const [paramValue, setValue] = useState("");
+    const [redirect, setRedirect] = useState(false);
     const difficulties:string[] = ["Easy", "Medium", "Hard"];
 
-    return(<div>
-        {mapButtons(difficulties, isDisabled, setDisabled, setValue)}
+    //Enable the Trivia Start button if it is currently disabled,
+    //Set the value of that trivia button to whatever radio button is enabled
+    function enableStart(value:string){
+        if(isDisabled){
+            setDisabled(false);
+        }
+        setValue(value.toLowerCase());
+        console.log(value);
+    }
 
-        <button id = 'startTriviaBtn' className = 'btn btn-secondary btn-lg' disabled= {isDisabled}  value= {propValue}>  Get Trivia!  </button>
+    //Put all of the difficulty buttons on screen using a map
+    const mapButtons = (difficulties: string[]) => (
+        difficulties.map((difficultySetting: string) => (
+            <div style = {{margin: 12}} key={difficultySetting + "Div"} >
+                <br></br>
+                <DifficultyButton 
+                key={difficultySetting} 
+                difficulty={difficultySetting} 
+                bootstrapColor={"info" }
+                onChange = {() => enableStart(difficultySetting)}
+                />
+                <br></br>
+            </div>
+        ))
+    )
+
+    const renderRedirect = () => {
+        if (redirect) {
+          return <Redirect to={'/game/' + paramValue.toLowerCase()} />
+        }
+      }
+
+    return(<div>
+        {renderRedirect()}
+        {mapButtons(difficulties)}
+        <button id = 'startTriviaBtn' className = 'btn btn-secondary btn-lg' disabled= {isDisabled}  value= {paramValue} onClick={() => setRedirect(true)}>  Get Trivia!  </button>
     </div>)
 }
 
-//Enable the Trivia Start button
-//Set the value of that trivia button to whatever radio button is enabled
-function enableStart(state: any, setDisabledCallback:(param:boolean) => void, difficulty:string,  value:string, setValueCallback:(value:string) => void){
-    if(state){
-          setDisabledCallback(false);
-    }
-    setValueCallback(value.toLowerCase());
-    console.log(value);
-}
 
-//Put all of the difficulty buttons on screen using a map
-const mapButtons = (difficulties: string[], state: any, callback:(value:boolean) => void , setValueCallback:(value:string) => void) => (
-    difficulties.map((difficultySetting: string) => (
-        <div style = {{margin: 12}} key={difficultySetting + "Div"} >
-            <br></br>
-            <DifficultyButton 
-            key={difficultySetting} 
-            difficulty={difficultySetting} 
-            bootstrapColor={"info" }
-            onChange = {() => enableStart(state, callback, difficultySetting, difficultySetting, setValueCallback)}
-            />
-            <br></br>
-        </div>
-    ))
-)
+
+
 
 // () => getQuestion(difficultySetting)
 
