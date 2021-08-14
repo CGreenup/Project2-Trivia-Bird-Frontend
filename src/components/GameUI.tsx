7c91f953c6cba462bdeef71ccae0f5cf69328f6f
June Greenup <cgreenup2000@gmail.com>
2021-08-14 17:35:03 -0500

    Question now only gets one question regardless of renders

--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -7,25 +7,42 @@ const GameUI:React.FC<unknown> = () => {
     const params:any  = useParams();
     const difficulty:string = params.difficulty
 
-    let question;
-    let questionData;
+    const [questionString, setQuestion] = useState("");
+    const [renders, setRenders] = useState(0);
 
     const getData = async () => {
+        setRenders(renders + 1);
         let data:any = await getQuestion(difficulty);
-        questionData = data;
+        let questionData = data;
         console.log(questionData);
-        question = questionData.results[0].question;
+        let question:string = questionData.results[0].question;
+        question = question.replaceAll("&#039;", "'");
+        question = question.replaceAll("&quot;", '"');
         
+        setQuestion(question);
+
         console.log("GameUI");
         console.log(questionData.results[0].question)
         console.log(question);
     }
     
-    
+    const getQuestionElement = () => {
+        console.log("Getting question!")
+        if(renders <= 0){
+            getData();
+            setRenders(renders + 1);
+            setQuestion("Loading question...")
+        }
+        return (
+            <div>
+                Question: {questionString}
+            </div>
+        )
+    }
 
     return (
         <div>
-            <button onClick={() => getData()}>get this</button>
+            {getQuestionElement()}
             <p>Quiz questions brought to you by Open Trivia Database by PIXELTAIL GAMES LLC</p>
         </div>
     )
