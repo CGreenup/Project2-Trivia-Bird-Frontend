6a6cbd889876db4937046501090d2ab2cb0c8b7b
June Greenup <cgreenup2000@gmail.com>
2021-08-15 00:34:28 -0500

    question buttons added

--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -9,7 +9,7 @@ const GameUI:React.FC<unknown> = () => {
     const difficulty:string = params.difficulty
 
     const [questionString, setQuestion] = useState("Loading Question...");
-    const [answersArray, setAnswers] = useState(Array<string>())
+    const [answersArray, setAnswers] = useState(Array<string>());
     const [correctAnswer, setCorrectAnswer] = useState("");
     const [renders, setRenders] = useState(0);
 
@@ -28,8 +28,8 @@ const GameUI:React.FC<unknown> = () => {
     }
 
     async function getData () {
-        console.log("===============")
-        console.log("In getData")
+        console.log("===============");
+        console.log("In getData");
 
         let data:any = await getQuestion(difficulty);
         console.log(data);
@@ -40,10 +40,16 @@ const GameUI:React.FC<unknown> = () => {
         let answers:string[] = Array<string>();
         answers = [...data.results[0].incorrect_answers, data.results[0].correct_answer];
         answers.sort();
+        setAnswers([...answers]);
+
+        let correct_answer:string = data.results[0].correct_answer;
+
+        setCorrectAnswer(correct_answer);
 
         console.log(data.results[0].question)
-        answers.map((ans:string) => {console.log(ans)} )
+        // answersArray.map((ans:string) => {console.log(ans)} )
         console.log(question);
+        console.log("===============");
     }
     
     const getQuestionInformation = () => {
@@ -54,9 +60,29 @@ const GameUI:React.FC<unknown> = () => {
         }
     }
 
+    function mapData(answers:string[]){
+        return (
+            <div className="row row-cols-2" style = {{margin:20}}>
+                {answers.map( (answer:string) => {
+                    return(<div className = "col py-1 px-1">
+                        <button className = "btn btn-lg btn-outline-info form-control">
+                            -
+                            <br></br>
+                            {answer}
+                            <br></br>
+                            -
+                        </button>
+                    </div>)
+                })}
+            </div>
+        )
+    }
+
     return (
         <div>
+            <br/>
             <h2>{questionString}</h2>
+            {mapData(answersArray)}
             <p>Quiz questions brought to you by <i>Open Trivia Database</i> by <b>PIXELTAIL GAMES LLC</b></p>
         </div>
     )
