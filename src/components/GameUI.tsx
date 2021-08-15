1ed70eab66c187762a86df8f363cb32e5035f378
June Greenup <cgreenup2000@gmail.com>
2021-08-14 19:54:01 -0500

    NOW it only gets one question regarldess of renders

--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestion } from "../remote/questionApi";
@@ -10,39 +11,43 @@ const GameUI:React.FC<unknown> = () => {
     const [questionString, setQuestion] = useState("");
     const [renders, setRenders] = useState(0);
 
-    const getData = async () => {
-        setRenders(renders + 1);
+    useEffect( () => {
+        getQuestionElement();
+    }, [] )
+
+    async function getData () {
+        console.log("===============")
+        console.log("In getData")
+        console.log("getting question data")
         let data:any = await getQuestion(difficulty);
-        let questionData = data;
-        console.log(questionData);
-        let question:string = questionData.results[0].question;
+        console.log(data);
+        let question:string = data.results[0].question;
         question = question.replaceAll("&#039;", "'");
         question = question.replaceAll("&quot;", '"');
+        question = question.replaceAll("&amp;", "&")
         
         setQuestion(question);
 
-        console.log("GameUI");
-        console.log(questionData.results[0].question)
+        console.log(data.results[0].question)
         console.log(question);
     }
     
     const getQuestionElement = () => {
-        console.log("Getting question!")
-        if(renders <= 0){
-            getData();
+        console.log("Getting question element!")
+        if(renders < 1){
             setRenders(renders + 1);
+            console.log("Renders are less than 1, so I'm getting the data Renders:"+renders)
+            getData();
             setQuestion("Loading question...")
         }
-        return (
-            <div>
-                Question: {questionString}
-            </div>
-        )
+        return "Question:" +  questionString;
+            
+        
     }
 
     return (
         <div>
-            {getQuestionElement()}
+            <h2>{questionString}</h2>
             <p>Quiz questions brought to you by Open Trivia Database by PIXELTAIL GAMES LLC</p>
         </div>
     )
