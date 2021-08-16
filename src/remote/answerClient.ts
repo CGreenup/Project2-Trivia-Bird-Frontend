9ff91d7a2116f261b6cdecc1dce24413076d348f
hwu5542 <hwu5542@gmail.com>
2021-08-16 03:18:01 -0500

    done and done

--- src/remote/answerClient.ts
+++ src/remote/answerClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const answerClient = axios.create({
-    baseURL:"http://localhost:8080/project2/",
+    // baseURL:"http://localhost:8080/project2/",
+    baseURL:'http://18.116.88.5:8080/project2-0.0.1-SNAPSHOT/',
     headers: {
         'Content-Type':'application/json',
       }
